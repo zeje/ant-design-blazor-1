be5c90ae9395c48e580e0c81451838fe26741cba
Benjamin Vertonghen <benjamin.vertonghen@apped.be>
2020-03-23 16:11:08 +0100

    Added Documentations
    ...

---
+++ docs/Append.AntDesign.Documentation/Shared/Codebox.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Append.AntDesign.Documentation.Shared
+{
+    public partial class Codebox
+    {
+        [Parameter] public RenderFragment Demo { get; set; }
+        [Parameter] public string Title { get; set; }
+        [Parameter] public string Description { get; set; }
+    }
+}
---
+++ docs/Append.AntDesign.Documentation/Shared/DemoPage.razor.cs
@@ -0,0 +1,24 @@
+﻿using Markdig;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Append.AntDesign.Documentation.Shared
+{
+    public partial class DemoPage
+    {
+        [Parameter] public string ComponentName { get; set; }
+        public MarkupString LoadMarkdownDocumentation()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = $"Append.AntDesign.Documentation.Components.{ComponentName}.Index.md";
+
+            using Stream stream = assembly.GetManifestResourceStream(resourceName);
+            using StreamReader reader = new StreamReader(stream);
+
+            string result = reader.ReadToEnd();
+            return (MarkupString)Markdown.ToHtml(result, new MarkdownPipelineBuilder().UseAdvancedExtensions().Build());
+        }
+    }
+}
