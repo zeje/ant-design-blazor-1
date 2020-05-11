978797e78fc3179d2cc0719c1036b4834cbd3694
Joris Claeys <joris.claeys12@gmail.com>
2020-05-11 18:47:52 +0200

    [InputNumber] Base of InputNumber + docs + tests

---
+++ docs/Append.AntDesign.Documentation/Components/InputNumber/Index.md
@@ -0,0 +1,32 @@
+﻿Enter a number within certain range with the mouse or keyboard.
+
+## When To Use
+
+When a numeric value needs to be provided.
+
+## API
+
+| Property | Description | Type | Default |
+| --- | --- | --- | --- |
+| autoFocus | get focus when component mounted | boolean | false |
+| defaultValue | initial value | number |  |
+| disabled | disable the input | boolean | false |
+| formatter | Specifies the format of the value presented | function(value: number \| string): string |  |
+| max | max value | number | Infinity |
+| min | min value | number | -Infinity |
+| parser | Specifies the value extracted from formatter | function( string): number |  |
+| precision | precision of input value | number |  |
+| decimalSeparator | decimal separator | string |  |
+| size | height of input box | `large` \| `middle` \| `small` |  |
+| step | The number to which the current value is increased or decreased. It can be an integer or decimal. | number\|string | 1 |
+| type | HTML inputs can have a type of `number`, and this can be added to aid mobile broswer keyboards to show the number keybaord, as well as limit inputs to numbers only `0-9` and `e`, but will not guaruntee client and server side validation. | string - ie 'number' |  |
+| value | current value | number |  |
+| onChange | The callback triggered when the value is changed. | function(value: number \| string) |  |
+| onPressEnter | The callback function that is triggered when Enter key is pressed. | function(e) |  |
+
+## Methods
+
+| Name    | Description  |
+| ------- | ------------ |
+| blur()  | remove focus |
+| focus() | get focus    |
---
+++ source/Append.AntDesign/Components/InputNumber/InputNumber.razor.cs
@@ -0,0 +1,117 @@
+﻿using Append.AntDesign.Core;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Append.AntDesign.Components
+{
+    public partial class InputNumber
+    {
+        private string _format;
+        private static readonly string prefix = "ant-input-number";
+
+        [Parameter] public double? DefaultValue { get; set; }
+        [Parameter] public bool Disabled { get; set; }
+        [Parameter] public Func<double, string> Formatter { get; set; }
+        [Parameter] public double Max { get; set; } = double.PositiveInfinity;
+        [Parameter] public double Min { get; set; } = double.NegativeInfinity;
+        [Parameter] public Func<string, double> Parser { get; set; }
+
+        [Parameter] public InputSize Size { get; set; } = InputSize.Middle;
+        private double _step = 1;
+        [Parameter]
+        public double Step
+        {
+            get
+            {
+                return _step;
+            }
+            set
+            {
+                _step = value;
+                if (string.IsNullOrEmpty(_format))
+                {
+                    _format = string.Join('.', _step.ToString().Split('.').Select(n => new string('0', n.Length)));
+                }
+            }
+        }
+        [Parameter] public double Value { get; set; }
+
+        private ClassBuilder classes => ClassBuilder.Create(Class)
+                .AddClass(prefix)
+                .AddClassWhen($"{prefix}-disabled", Disabled)
+                .AddClassWhen($"{prefix}-lg", Size == InputSize.Large)
+                .AddClassWhen($"{prefix}-sm", Size == InputSize.Small);
+
+        protected string inputHandlerPrefix = $"{prefix}-handler"; 
+
+        private ClassBuilder iconClassesUp => ClassBuilder.Create(Class)
+                .AddClass(inputHandlerPrefix)
+                .AddClass($"{inputHandlerPrefix}-up")
+                .AddClassWhen($"{inputHandlerPrefix}-up-disabled", Value >= Max)
+                .AddClassWhen($"{inputHandlerPrefix}-up-disabled", Disabled);
+
+        private ClassBuilder iconClassesDown => ClassBuilder.Create(Class)
+                .AddClass(inputHandlerPrefix)
+                .AddClass($"{inputHandlerPrefix}-down")
+                .AddClassWhen($"{inputHandlerPrefix}-down-disabled", Value <= Min)
+                .AddClassWhen($"{inputHandlerPrefix}-down-disabled", Disabled);
+
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            if (DefaultValue.HasValue)
+            {
+                Value = DefaultValue.Value;
+            }
+        }
+
+        private void Decrease()
+        {
+            OnInput(new ChangeEventArgs() { Value = Value - Step });
+        }
+        private void Increase()
+        {
+            OnInput(new ChangeEventArgs() { Value = Value + Step });
+        }
+
+        private void OnInput(ChangeEventArgs args)
+        {
+            double num;
+            if (Parser != null)
+            {
+                num = Parser(args.Value.ToString());
+            }
+            else
+            {
+                _ = double.TryParse(args.Value.ToString(), out num);
+            }
+
+            if (num >= Min && num <= Max)
+            {
+                Value = num;
+            } else if(num < Min)
+            {
+                Value = Min;
+            } else if(num > Max)
+            {
+                Value = Max;
+            }
+        }
+
+        private string DisplayValue()
+        {
+            if (Formatter != null)
+            {
+                return Formatter(Value);
+            }
+
+            return Value.ToString(_format);
+        }
+    }
+}
