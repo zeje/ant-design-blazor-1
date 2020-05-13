44b24cefbd02dad3978f6db6cd85fcf564a8caca
Joris Claeys <joris.claeys12@gmail.com>
2020-05-13 20:34:16 +0200

    [Steps] Progress
    Start with Step and add stepstatus

--- source/Append.AntDesign/Components/Steps/Step.razor.cs
+++ source/Append.AntDesign/Components/Steps/Step.razor.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Append.AntDesign.Core;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +9,20 @@ namespace Append.AntDesign.Components
 {
     public partial class Step
     {
-    
+        [Parameter] public string Description { get; set; } = string.Empty;
+        [Parameter] public IconType Icon { get; set; }
+        [Parameter] public StepStatus Status { get; set; } = StepStatus.Wait;
+        [Parameter] public string Title { get; set; } = string.Empty;
+        [Parameter] public string Subtitle { get; set; } = string.Empty;
+        [Parameter] public bool Disabled { get; set; }
+        [Parameter] public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+        string prefix = "ant-steps-item";
+
+        private ClassBuilder classes => ClassBuilder.Create(prefix)
+               .AddClass($"{prefix}-{Status}")
+               .AddClassWhen($"{prefix}-disabled", Disabled)
+               .AddClassWhen($"{prefix}-custom", Icon != null);
+
     }
 }
---
+++ source/Append.AntDesign/Components/Steps/StepStatus.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.SmartEnum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Append.AntDesign.Components
+{
+    public sealed class StepStatus : SmartEnum<TagStatus>
+    {
+        public static readonly StepStatus Wait = new StepStatus(nameof(Wait).ToLower(), 1);
+        public static readonly StepStatus Process = new StepStatus(nameof(Process).ToLower(), 2);
+        public static readonly StepStatus Finish = new StepStatus(nameof(Finish).ToLower(), 3);
+        public static readonly StepStatus Error = new StepStatus(nameof(Error).ToLower(), 4);
+
+        private StepStatus(string name, int value) : base(name, value)
+        {
+        }
+    }
+}
